7f41b479cb99bff3e0cfd076497dfbc358c73b8b
Tui Tpk <tanamet.tpk@gmail.com>
2019-10-16 10:11:11 +0700

    / class to hook style

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import KaptuerHeader from '../components/custom/KaptuerHeader'
 import {Button} from '../components/Button'
 import Section from '../components/Section'
@@ -10,10 +10,10 @@ import GoogleLogin from 'react-google-login'
 import FacebookLogin from 'react-facebook-login'
 import { TitleSeperateLine } from '../components/SeperateLine'
 import LoginForm from '../components/custom/LoginForm'
-import { FB_TOKEN, GOOGLE_TOKEN } from '../../configs/oauth'
-import signable from '../../util/api/libs/signable'
-import { logIn } from '../../store/actions/user'
-import { useFormHandle } from '../../util/hooks'
+import { FB_TOKEN, GOOGLE_TOKEN } from '../configs/oauth'
+import signable from '../util/api/libs/signable'
+// import { logIn } from '../store/actions/user'
+import { useFormHandle, useScrollY } from '../util/hooks'
 import '../assets/css/main.css'
 
 // get information to display in that language
@@ -69,75 +69,70 @@ let buttons = [
     },
 ]
 
-export default class LandingPage extends Component {
-
-    constructor(props){
-
-        super(props)
-
-        this.state = {
-            currentY:0,
-            email:"",
-            modalSingupIsOpen: false,
-            modalLoginIsOpen: false,
-        }
-
+const LandingPage = (props) => {
+    
+    let currentY = 0
+    if (process.browser) {
+        // client-side-only code
+        currentY = useScrollY()
     }
 
-    componentDidMount(){
-        window.addEventListener('scroll', this.handleScroll);
-    }
+    let [state, setState] = useState({
+        email:"",
+        modalSingupIsOpen: false,
+        modalLoginIsOpen: false,
+    })
 
-    componentWillUnmount(){
-        window.removeEventListener('scroll', this.handleScroll);
-    }
+    // const [user, handleFormChange] = useFormHandle({
+    //     email: "",
+    //     password: "",
+    // });
+    
+    // const login = async (payload, method) => {
+    //     if (await props.logIn(signable[method](payload))) {
+    //         history.push("/applications")
+    //     }
+    // }
 
-    handleScroll = (e) => {
-
-        let currentY = window.scrollY;
-        this.setState({currentY})
-        
-    }
-
-    headerButtonOnClick = ( key ) => {
+    const headerButtonOnClick = ( key ) => {
         
         if(key === "signup"){
             // open sign-up modal
-            this.openSignupModal()
+            openSignupModal()
         }
         else if(key === "login"){
-            this.openLoginModel()
+            openLoginModel()
         }
 
     }
 
-    onInputEmailChange = ( e ) => {
+    const onInputEmailChange = ( e ) => {
         
         const email = e.target.value
         
-        this.setState({email})
+        setState({...state, email})
         
     }
 
-    openLoginModel = () => {
-        this.closeSignupModal()
-        this.setState({modalLoginIsOpen: true});
+    const openLoginModel = () => {
+        closeSignupModal()
+        setState({...state, modalLoginIsOpen: true});
     }
 
-    closeLoginModal = () => {
-        this.setState({modalLoginIsOpen: false});
+    const closeLoginModal = () => {
+        setState({...state, modalLoginIsOpen: false});
     }
 
-    openSignupModal = () => {
-        this.closeLoginModal()
-        this.setState({modalSingupIsOpen: true});
+    const openSignupModal = () => {
+        closeLoginModal()
+        setState({...state, modalSingupIsOpen: true});
     }
 
-    closeSignupModal = () => {
-        this.setState({modalSingupIsOpen: false});
+    const closeSignupModal = () => {
+        setState({...state, modalSingupIsOpen: false});
     }
 
-    login = (res, type) => {
+    const login = (res, type) => {
         // define
         let email = null;
         let password = null;
@@ -176,124 +171,123 @@ export default class LandingPage extends Component {
         let payload = { email, name, token, picture , password , type }
         console.log(payload);
 
-    };
+    }
 
-    render() {
+    // check scrolling for display shadow of header
+    const shadow = currentY <= 50 ? false : true
 
-        // check scrolling for display shadow of header
-        const shadow = this.state.currentY <= 50 ? false : true
+    return (
+        <div className="Landing-page" >
 
-        return (
-            <div className="Landing-page" >
+            {/* custom header */}
+            <KaptuerHeader logo="/logo.svg" action={headerButtonOnClick} buttons={buttons} shadow={shadow} />
 
-                {/* custom header */}
-                <KaptuerHeader logo="/logo.svg" action={this.headerButtonOnClick} buttons={buttons} shadow={shadow} />
+            {/* first section */}
+            <Section backgroundColor="white" height="100vh" >
+                <Stack minWidth={"450px"} maxWidth={"50%"} >
 
-                {/* first section */}
-                <Section backgroundColor="white" height="100vh" >
-                    <Stack minWidth={"450px"} maxWidth={"50%"} >
+                    {/* slogan of organize */}
+                    <div style={{paddingBottom:"37px" , fontSize:"3rem"}}>
+                        {information.LANDING_MAIN_SLOGAN}
+                    </div>
+                    <div style={{paddingBottom:"37px" , color:"gray"}}>
+                        {information.LANDING_MAIN_DEF}
+                    </div>
 
-                        {/* slogan of organize */}
-                        <div style={{paddingBottom:"37px" , fontSize:"3rem"}}>
-                            {information.LANDING_MAIN_SLOGAN}
-                        </div>
-                        <div style={{paddingBottom:"37px" , color:"gray"}}>
-                            {information.LANDING_MAIN_DEF}
-                        </div>
-
-                        {/* main input */}
-                        <div className="Login-signup-form">
-                            <Input placeholder="name@company.com" onChange={this.onInputEmailChange} />
-                            <Button customStyle={{ margin: "0px 12px" }} href="/signup" text={information.LANDING_MAIN_SIGNUP_BUTTON} primary />
-                        </div>
-
-                    </Stack>
-                </Section>
-
-                {/* signup modal */}
-                <Modal
-                    closeTimeoutMS={180}
-                    isOpen={this.state.modalSingupIsOpen}
-                    onRequestClose={this.closeSignupModal}
-                    contentLabel="Example Modal"
-                    className="Modal-signup"
-                    overlayClassName="Modal-Overlay-login"
-                >
-
-                    {/* modal information */}
-                    <Stack minWidth={"450px"} maxWidth={"70%"} >
-
-                        <div style={{paddingBottom:"37px" , fontSize:"3rem" , textAlign:'center'}}>
-                            {information.LANDING_MODAL_TITLE}
-                        </div>
-
-                        <div style={{paddingBottom:"37px" , color:"gray" , textAlign:'center'}}>
-                            {information.LANDING_MODAL_DESCRIPTION}
-                        </div>
-
-                    </Stack>
-
-                    {/* modal input */}
+                    {/* main input */}
                     <div className="Login-signup-form">
-                        <Input placeholder="name@company.com" onChange={this.onInputEmailChange} />
+                        <Input placeholder="name@company.com" onChange={onInputEmailChange} />
                         <Button customStyle={{ margin: "0px 12px" }} href="/signup" text={information.LANDING_MAIN_SIGNUP_BUTTON} primary />
                     </div>
 
-                    {/* modal policy part */}
-                    <Stack minWidth={"450px"} maxWidth={"50%"} >
+                </Stack>
+            </Section>
 
-                        <div style={{paddingTop:"37px" , color:"gray" , textAlign:'center'}}>
-                            <p>
-                                {information.LANDING_MODAL_POLICIES[0]} <a className="hilightLink" href="/">Privacy Policy</a> {information.LANDING_MODAL_POLICIES[1]} <a className="hilightLink" href="/" >Terms of Service.</a>
-                            </p>
-                            
-                        </div>
+            {/* signup modal */}
+            <Modal
+                closeTimeoutMS={180}
+                isOpen={state.modalSingupIsOpen}
+                onRequestClose={closeSignupModal}
+                contentLabel="Example Modal"
+                className="Modal-signup"
+                overlayClassName="Modal-Overlay-login"
+            >
 
-                    </Stack>
+                {/* modal information */}
+                <Stack minWidth={"450px"} maxWidth={"70%"} >
 
-                </Modal>
+                    <div style={{paddingBottom:"37px" , fontSize:"3rem" , textAlign:'center'}}>
+                        {information.LANDING_MODAL_TITLE}
+                    </div>
 
-                {/* Login modal */}
-                <Modal
-                    closeTimeoutMS={180}
-                    isOpen={this.state.modalLoginIsOpen}
-                    onRequestClose={this.closeLoginModal}
-                    className="Modal-login"
-                    overlayClassName="Modal-Overlay-login"
-                >
+                    <div style={{paddingBottom:"37px" , color:"gray" , textAlign:'center'}}>
+                        {information.LANDING_MODAL_DESCRIPTION}
+                    </div>
 
-                    {/* Title of modal */}
-                    <label style={{fontSize:"2.5rem"}} >Log In</label>
+                </Stack>
 
-                    {/* OAuth With other Provider */}
-                    <GridCol gap="20px" customStyle={{margin:"25px 0px"}} >
+                {/* modal input */}
+                <div className="Login-signup-form">
+                    <Input placeholder="name@company.com" onChange={onInputEmailChange} />
+                    <Button customStyle={{ margin: "0px 12px" }} href="/signup" text={information.LANDING_MAIN_SIGNUP_BUTTON} primary />
+                </div>
 
-                        <FacebookLogin
-                            appId="458064678311812"
-                            autoLoad={false}
-                            fields="name,email,picture"
-                            callback={(res)=>this.login(res,"facebook")} />
+                {/* modal policy part */}
+                <Stack minWidth={"450px"} maxWidth={"50%"} >
 
-                        <GoogleLogin
-                            clientId="334399017995-ciok6m57onhc5u54o7sqdamhi00agt7a.apps.googleusercontent.com"
-                            buttonText="Login"
-                            onSuccess={(res)=>this.login(res,"google")}
-                            cookiePolicy={'single_host_origin'} />
+                    <div style={{paddingTop:"37px" , color:"gray" , textAlign:'center'}}>
+                        <p>
+                            {information.LANDING_MODAL_POLICIES[0]} <a className="hilightLink" href="/">Privacy Policy</a> {information.LANDING_MODAL_POLICIES[1]} <a className="hilightLink" href="/" >Terms of Service.</a>
+                        </p>
+                        
+                    </div>
 
-                    </GridCol>
-                    
-                    {/* Seperate Line */}
-                    <TitleSeperateLine title="OR" />
+                </Stack>
 
-                    {/* Login form */}
-                    <LoginForm onSubmit={(res)=>this.login(res,"local")} />
+            </Modal>
 
-                    {/* switch to signup modal */}
-                    <p style={{color:"gray"}}>Don’t have an account? <label className="hilightLink" onClick={this.openSignupModal} >Sign up</label></p>
+            {/* Login modal */}
+            <Modal
+                closeTimeoutMS={180}
+                isOpen={state.modalLoginIsOpen}
+                onRequestClose={closeLoginModal}
+                className="Modal-login"
+                overlayClassName="Modal-Overlay-login"
+            >
 
-                </Modal>
+                {/* Title of modal */}
+                <label style={{fontSize:"2.5rem"}} >Log In</label>
 
-            </div>
-        )
-    }
+                {/* OAuth With other Provider */}
+                <GridCol gap="20px" customStyle={{margin:"25px 0px"}} >
+
+                    <FacebookLogin
+                        appId="458064678311812"
+                        autoLoad={false}
+                        fields="name,email,picture"
+                        callback={(res)=>login(res,"facebook")} />
+
+                    <GoogleLogin
+                        clientId="334399017995-ciok6m57onhc5u54o7sqdamhi00agt7a.apps.googleusercontent.com"
+                        buttonText="Login"
+                        onSuccess={(res)=>login(res,"google")}
+                        cookiePolicy={'single_host_origin'} />
+
+                </GridCol>
+                
+                {/* Seperate Line */}
+                <TitleSeperateLine title="OR" />
+
+                {/* Login form */}
+                <LoginForm onSubmit={(res)=>login(res,"local")} />
+
+                {/* switch to signup modal */}
+                <p style={{color:"gray"}}>Don’t have an account? <label className="hilightLink" onClick={openSignupModal} >Sign up</label></p>
+
+            </Modal>
+
+        </div>
+    )
 }
+
+export default LandingPage
