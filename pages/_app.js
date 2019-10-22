59febd9a5d3454b34362e3f73692528484b70f16
Tui Tpk <tanamet.tpk@gmail.com>
2019-10-22 17:15:59 +0700

    + favicon, Loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Provider} from "react-redux";
 import App from "next/app";
+import Head from 'next/head'
 import withRedux from "next-redux-wrapper";
 import {makeStore} from "../store";
 import Storage from '../util/api/libs/jwtStorage'
 import API from '../configs/apis'
 
-if (Storage.load()) window.location.href = API.APP;
+// if (Storage.load()) window.location.href = API.APP;
 
 class MyApp extends App {
 
@@ -24,6 +25,12 @@ class MyApp extends App {
         const {Component, pageProps, store} = this.props;
         return (
             <Provider store={store}>
+                <Head>
+                    <title>Kaptuer</title>
+                    <meta charSet="utf-8" />
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+                </Head>
                 <Component {...pageProps} />
             </Provider>
         );
