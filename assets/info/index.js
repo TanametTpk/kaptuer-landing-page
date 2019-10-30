c570edbf3a1fb876726158a0f934ff02e1cfdcba
Tui Tpk <tanamet.tpk@gmail.com>
2019-10-30 18:12:38 +0700

    Deploy Next.js to gh-pages

--- assets/info/index.js
+++
@@ -1,8 +0,0 @@
-const th = require('./language-th');
-const en = require('./language-en');
-
-
-module.exports =  {
-    th : th,
-    en : en
-}
--- store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import user from './user'
-// ADD reducer here
-
-export default combineReducers({
-    user,
-
-})
